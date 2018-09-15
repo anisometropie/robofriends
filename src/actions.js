081c1e35e04692aba2e935dee0c132453d288448
ut <ut@Albizia>
2018-09-15 20:06:24 +0200

    redux stuff

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,20 @@
-import { CHANGE_SEARCH_FIELD } from 'constants.js'
+import {
+	CHANGE_SEARCH_FIELD,
+	REQUEST_REBOTS_PENDING,
+	REQUEST_REBOTS_SUCCESS,
+	REQUEST_REBOTS_FAILED
+ } from 'constants.js'
 
 export const setSearchField = (text) => ({
 	type: CHANGE_SEARCH_FIELD,
 	payload: text
 });
+
+// returns a function instead of an object, to let redux-thunk know there is something to do.
+export const requestRobots = () => (dispatch) => {
+	dispatch({ type: REQUEST_REBOTS_PENDING });
+	fetch('https://jsonplaceholder.typicode.com/users')
+		.then( response => response.json() )
+		.then(data => dispatch({ type: REQUEST_REBOTS_SUCCESS, payload: data }))
+		.catch(error => dispatch({ type: REQUEST_REBOTS_FAILED, payload: error}));
+};
--- src/reducers.js
+++ src/reducers.js
@@ -1,15 +1,39 @@
-import { CHANGE_SEARCH_FIELD } from 'constants.js'
+import {
+	CHANGE_SEARCH_FIELD,
+	REQUEST_REBOTS_PENDING,
+	REQUEST_REBOTS_SUCCESS,
+	REQUEST_REBOTS_FAILED
+ } from 'constants.js'
 
 
-const initialState = {
+const initialStateSearch = {
 	searchField: ''
 }
 
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialStateSearch, action={}) => {
 	switch(action.type) {
 		case CHANGE_SEARCH_FIELD:
 			return Object.assign({}, state, {searchField: action.payload});
 			// return { ...state, searchField: action.payload };
+		default:
+			return state;
+	}
+}
+
+const initialStateRobots = {
+	isPending: false,
+	robots: [],
+	error: ''
+}
+
+export const requestRobots = (state=initialStateRobots, action={}) => {
+	switch(action.type) {
+		case REQUEST_REBOTS_PENDING:
+			return Object.assign({}, state, {isPending: true});
+		case REQUEST_REBOTS_SUCCESS:
+			return Object.assign({}, state, {robots: action.payload, isPending: false});
+		case REQUEST_REBOTS_FAILED:
+			return Object.assign({}, state, {error: action.payload, isPending: false});
 		default:
 			return state;
 	}
